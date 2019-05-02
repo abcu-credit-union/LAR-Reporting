042f652881a500c8706e3c31dfd5eb3e3287af45
ZacWhite <zac.white13@gmail.com>
2019-05-02 11:38:30 -0600

    Update Cash Inflows.m
    - Rebuilt the query so total balances accurately reflect line 141 on the F&S

--- Queries/Cash Inflows.m
+++ Queries/Cash Inflows.m
@@ -13,42 +13,36 @@ let
 	SQL =
 "SELECT
     ACCT.BRANCHORGNBR,
-	WH_ACCTCOMMON.ACCTNBR,
-	BalSubAcct.SUBACCTNBR,
+    WH_ACCTCOMMON.ACCTNBR,
+    ACCTSUBACCT.SUBACCTNBR,
 	WH_ACCTCOMMON.EFFDATE,
 	WH_ACCTCOMMON.MJACCTTYPCD,
-	WH_ACCTCOMMON.NOTEBAL,
-	WH_ACCTCOMMON.CURRACCTSTATCD,
-	WH_ACCTCOMMON.CURRMIACCTTYPCD,
-	BalSubAcct.BALCATCD,
-	BalSubAcct.BALTYPCD,
+    WH_ACCTCOMMON.CURRMIACCTTYPCD,
+    ACCTSUBACCT.BALCATCD,
+    SUBSTR(GLACCT.XREFGLACCTNBR, 0, 8) ""GL Number"",
+    SUBSTR(GLACCT.XREFGLACCTNBR, -3) ""Cost Center"",
 	COALESCE(CEIL(MONTHS_BETWEEN(ACCT.DATEMAT, TO_DATE('"&StartDate&"','MM/DD/YYYY'))), 0) AS RemainingAmortization,
 	COALESCE(WH_ACCTLOAN.TOTALPI, 0) AS TOTALPI,
 	WH_ACCTCOMMON.TAXRPTFORPERSNBR,
 	WH_ACCTCOMMON.TAXRPTFORORGNBR
 FROM WH_ACCTCOMMON
-INNER JOIN
-	(SELECT
-        ACCTSUBACCT.ACCTNBR,
-        ACCTSUBACCT.SUBACCTNBR,
-        ACCT.MJACCTTYPCD,
-        ACCTSUBACCT.BALCATCD,
-        ACCTSUBACCT.BALTYPCD
-    FROM ACCTSUBACCT
-    INNER JOIN ACCT
-        ON ACCTSUBACCT.ACCTNBR = ACCT.ACCTNBR
-    WHERE (ACCT.MJACCTTYPCD NOT IN ('CK', 'SAV')
-            AND ACCTSUBACCT.BALTYPCD = 'BAL'
-            AND ACCTSUBACCT.BALCATCD = 'NOTE')
-        OR (ACCT.MJACCTTYPCD IN ('CK', 'SAV')
-            AND ACCTSUBACCT.BALTYPCD = 'BAL'
-            AND ACCTSUBACCT.BALCATCD IN ('LMOD', 'ODEX'))) BalSubAcct
-	ON WH_ACCTCOMMON.ACCTNBR = BalSubAcct.ACCTNBR
 INNER JOIN ACCT
     ON WH_ACCTCOMMON.ACCTNBR = ACCT.ACCTNBR
 LEFT OUTER JOIN WH_ACCTLOAN
     ON WH_ACCTCOMMON.ACCTNBR = WH_ACCTLOAN.ACCTNBR
     AND WH_ACCTCOMMON.EFFDATE = WH_ACCTLOAN.EFFDATE
+INNER JOIN ACCTSUBACCT
+    ON WH_ACCTCOMMON.ACCTNBR = ACCTSUBACCT.ACCTNBR
+    AND ACCTSUBACCT.BALTYPCD = 'BAL'
+INNER JOIN MJMIACCTGL
+    ON WH_ACCTCOMMON.MJACCTTYPCD = MJMIACCTGL.MJACCTTYPCD
+    AND WH_ACCTCOMMON.CURRMIACCTTYPCD = MJMIACCTGL.MIACCTTYPCD
+    AND ACCTSUBACCT.BALCATCD = MJMIACCTGL.BALCATCD
+    AND ACCTSUBACCT.BALTYPCD = MJMIACCTGL.BALTYPCD
+    AND MJMIACCTGL.INACTIVEDATE IS NULL
+INNER JOIN GLACCT
+    ON MJMIACCTGL.GLACCTTITLENBR = GLACCT.GLACCTTITLENBR
+    AND SUBSTR(GLACCT.XREFGLACCTNBR, -3) = ACCT.BRANCHORGNBR
 WHERE WH_ACCTCOMMON.EFFDATE = TO_DATE('"&StartDate&"', 'MM/DD/YYYY')
 	AND WH_ACCTCOMMON.CURRACCTSTATCD NOT IN  ('CLS', 'CO')
 	AND WH_ACCTCOMMON.NOTEBAL <> 0
@@ -67,87 +61,109 @@ WHERE WH_ACCTCOMMON.EFFDATE = TO_DATE('"&StartDate&"', 'MM/DD/YYYY')
 
 
     ABCUSource = Table.Combine({BBSource, CCSource}),
-
-    #"Added GL Numbers" = Table.ExpandTableColumn(
-        Table.NestedJoin(ABCUSource, {"BRANCHORGNBR", "MJACCTTYPCD", "CURRMIACCTTYPCD", "BALCATCD", "BALTYPCD"},
-        DNAGLMapping, {"COSTCENTER", "MJACCTTYPCD", "MIACCTTYPCD", "BALCATCD", "BALTYPCD"}, "GL", JoinKind.LeftOuter),
-    "GL", {"GLNUM", "GLACCTTITLENAME"}),
-    #"Added GL Balances" = Table.ExpandTableColumn(
-        Table.NestedJoin(#"Added GL Numbers", {"BRANCHORGNBR", "GLNUM"},
-        FASQuery, {"OrgShortName", "UserAcctNum"}, "GL Balance", JoinKind.LeftOuter),
-    "GL Balance", {"YTDBal", "FR2900Code"}),
 /*
-LIP GLs are not joined. The #"Adjusted for LIP" step modifies the YTDBal to accurately reflect what is reported on the
-F&S
+This section adds account balances from the SubAcctBalances table.
 */
-    #"Adjusted for LIP" = Table.FromRecords(
-        Table.TransformRows(#"Added GL Balances", (r) => Record.TransformFields(r,
-            {{"YTDBal", each if r[GLNUM] = 11501120 and r[BRANCHORGNBR] = 332 then
-                _ + LIPBalance
-            else _}}))
-    ),
+    #"Added Account Balances" = Table.ExpandTableColumn(
+        Table.NestedJoin(ABCUSource, {"ACCTNBR", "SUBACCTNBR"}, SubAcctBalances, {"ACCTNBR", "SUBACCTNBR"}, "BAL", JoinKind.Inner),
+    "BAL", {"BALAMT"}, {"Account Balance"}),
+
+    #"Changed Type" = Table.TransformColumnTypes(#"Added Account Balances",
+        {{"GL Number", type number},
+        {"Cost Center", type number}}),
 
 /*
-ACCTSUBACCTBAL is used to provide balances.  This table seperates balances into principal, accrued interest, etc.
-This is most reflective of the the "principal only balances" set out in NCCF
+FR2900 codes and FSMS line are added from the FAS query. This join is occuring on the GL number and cost center
 */
+    #"Added GL Codes" = Table.ExpandTableColumn(
+        Table.NestedJoin(#"Changed Type", {"GL Number", "Cost Center"},
+            FASQuery, {"UserAcctNum", "OrgShortName"},
+        "GL", JoinKind.LeftOuter),
+    "GL", {"FR2900Code", "FSMS Line"}),
+/*
+An ABS function is used to convert all values to a positive so that a weighted average can be used to convert DNA balances
+to FAS balances
+*/
+    #"Adjusted Account Balances" = Table.TransformColumns(#"Added GL Codes",
+        {{"Account Balance", each Number.Abs(_), type number}}),
 
-    #"Added Balances" = Table.ExpandTableColumn(
-        Table.NestedJoin(#"Adjusted for LIP", {"Source DB", "ACCTNBR", "SUBACCTNBR"},
-        SubAcctBalances, {"Source DB", "ACCTNBR", "SUBACCTNBR"}, "Balances", JoinKind.Inner),
-    "Balances", {"BALAMT"}),
-    #"Added Product Balances" = Table.ExpandTableColumn(
-        Table.NestedJoin(#"Added Balances", {"BRANCHORGNBR", "GLNUM"},
-        InflowsDNAProductBal, {"BRANCHORGNBR", "GLNUM"}, "ProductBal", JoinKind.LeftOuter),
-    "ProductBal", {"Product Balance"}),
+        /*
+        This is a substep that is totaling DNA balances based on the associated FR code and cost center
+        */
+
+        #"Grouped Rows" = Table.Group(#"Adjusted Account Balances", {"FR2900Code", "Cost Center"},
+            {{"Sum", each List.Sum([Account Balance]), type number}}),
 /*
-After IFRS9 data is uploaded a report is run that spits out loan stages and account numbers.
-The following code matches accounts to their IFRS 9 loan stage
+The totals are added into the main query during this step.
 */
-    #"Added loan stages" = Table.ExpandTableColumn(
-        Table.NestedJoin(#"Added Product Balances", {"Source DB", "ACCTNBR"},
-        IFRS9LoanStages, {"Source DB", "Account Number"}, "Stages", JoinKind.LeftOuter),
-    "Stages", {"Stage"}, {"IFRS9 Stage"}),
+    #"Added DNA GL Balances" = Table.ExpandTableColumn(
+        Table.NestedJoin(#"Adjusted Account Balances", {"FR2900Code", "Cost Center"},
+            #"Grouped Rows", {"FR2900Code", "Cost Center"},
+        "Prod Bal", JoinKind.LeftOuter),
+    "Prod Bal", {"Sum"}, {"DNA Balance"}),
+
+        /*
+        Another substep here. This is totaling the FAS balances based on the FR2900 codes
+        */
+        #"Grouped FR2900 Codes" = Table.Group(FASQuery, {"FR2900Code", "OrgShortName"},
+            {"Balance", each List.Sum([YTDBal]), type number}),
 /*
-An absolute function is used to translate negative balances (AOD's) to positive
+Adding FAS balances back to the main query
 */
-    #"Added FP Balance" = Table.AddColumn(#"Added loan stages", "FP Balance",
-        each (Number.Abs([BALAMT])/[Product Balance]) * [YTDBal]),
+    #"Added FR Balances" = Table.ExpandTableColumn(
+        Table.NestedJoin(#"Added DNA GL Balances", {"FR2900Code", "Cost Center"},
+            #"Grouped FR2900 Codes", {"FR2900Code", "OrgShortName"},
+        "FRBal", JoinKind.LeftOuter),
+    "FRBal", {"Balance"}, {"FR Balance"}),
 /*
-Calls a function that applies the correct F&S line number to the different cash inflows
+NCCF should only reflect principle balances of loan and lease recievables. This coresponds with line 141 on the F&S.
+The below step filters based on this
 */
-    #"Added Line Num" = Table.AddColumn(#"Added FP Balance", "Line Num",
+    #"Filtered Rows" = Table.SelectRows(#"Added FR Balances",
+        each ([FSMS Line] = "FP141")),
+/*
+Weighted average is used to convert DNA balances to principle only FAS balances
+*/
+    #"Added FP Balances" = Table.AddColumn(#"Filtered Rows", "FP Balance",
+        each ([Account Balance] / [DNA Balance]) * [FR Balance], type number),
+
+
+    #"Added Line Num" = Table.AddColumn(#"Added FP Balances", "Line Num",
         each fnInflowLine([MJACCTTYPCD], [CURRMIACCTTYPCD], [TAXRPTFORPERSNBR], [TAXRPTFORORGNBR])),
-/*
-A second function is used here to provide liquidity balances.  A function is used for these that assess each accounts information and
-determines what cash inflows to account for
-5/1/2019 ZW: Should be using FP Balance for the fnLiquidtyBalance function
-*/
-    #"Added Liquidity Balance" = Table.AddColumn(#"Added Line Num", "Liquidity Balance",
-        each fnLiquidityBalance([FP Balance], [Line Num], [IFRS9 Stage], [TOTALPI])),
-    #"Changed Type" = Table.TransformColumnTypes(#"Added Liquidity Balance",{{"Liquidity Balance", type number}}),
+
+    #"Added loan stages" = Table.ExpandTableColumn(
+        Table.NestedJoin(#"Added Line Num", {"Source DB", "ACCTNBR"},
+            IFRS9LoanStages, {"Source DB", "Account Number"},
+        "Stages", JoinKind.LeftOuter),
+    "Stages", {"Stage"}, {"IFRS9 Stage"}),
+
+    #"Added Liquidity Balance" = Table.AddColumn(#"Added loan stages", "Liquidity Balance",
+        each fnLiquidityBalance([FP Balance], [Line Num], [IFRS9 Stage], [TOTALPI]), type number),
+
 /*
 This function creates the cash inflows, and outflows based on the criteria set out in the F&S completition guidelines.
 A record is produced with each outflow grouped into its respective bucket, the next line of code expands those records.
 */
     #"Added Inflows" = Table.ExpandRecordColumn(
-        Table.AddColumn(#"Changed Type", "Records", each fnPayments([Line Num], [Liquidity Balance], [TOTALPI], [REMAININGAMORTIZATION])),
-    "Records", {"Month1", "Month2", "Month3", "Month4to6", "Month7to9", "Month10to12", "Month12Up"}),
+            Table.AddColumn(#"Added Liquidity Balance", "Records",
+                each fnPayments([Line Num], [Liquidity Balance], [TOTALPI], [REMAININGAMORTIZATION])),
+        "Records", {"Month1", "Month2", "Month3", "Month4to6", "Month7to9", "Month10to12", "Month12Up"}),
     #"Sorted Rows" = Table.Sort(#"Added Inflows",{{"ACCTNBR", Order.Descending}, {"BALCATCD", Order.Descending}}),
 /*
 This aggregates the information for reporting purposes.
 */
-    #"Grouped Rows" = Table.Group(#"Sorted Rows", {"EFFDATE", "Line Num"},
-        {{"FP BAL", each List.Sum([FP Balance]), type number},
-        {"LIQUIDITY BAL", each List.Sum([Liquidity Balance]), type number},
-        {"MTH 1", each List.Sum([Month1]), type number},
-        {"MTH 2", each List.Sum([Month2]), type number},
-        {"MTH 3", each List.Sum([Month3]), type number},
-        {"MTHS 4 TO 6", each List.Sum([Month4to6]), type number},
-        {"MTHS 7 TO 9", each List.Sum([Month7to9]), type number},
-        {"MTHS 10 TO 12", each List.Sum([Month10to12]), type number},
-        {"MTHS 12+", each List.Sum([Month12Up]), type number}}),
-    #"Sorted Rows1" = Table.Sort(#"Grouped Rows",{{"Line Num", Order.Ascending}}),
-    #"Changed Type1" = Table.TransformColumnTypes(#"Sorted Rows1",{{"EFFDATE", type date}})
+        #"Grouped Results" = Table.Group(#"Sorted Rows", {"EFFDATE", "Line Num"},
+            {{"FP BAL", each List.Sum([FP Balance]), type number},
+            {"LIQUIDITY BAL", each List.Sum([Liquidity Balance]), type number},
+            {"MTH 1", each List.Sum([Month1]), type number},
+            {"MTH 2", each List.Sum([Month2]), type number},
+            {"MTH 3", each List.Sum([Month3]), type number},
+            {"MTHS 4 TO 6", each List.Sum([Month4to6]), type number},
+            {"MTHS 7 TO 9", each List.Sum([Month7to9]), type number},
+            {"MTHS 10 TO 12", each List.Sum([Month10to12]), type number},
+            {"MTHS 12+", each List.Sum([Month12Up]), type number}}),
+        #"Sorted Rows1" = Table.Sort(#"Grouped Results",{{"Line Num", Order.Ascending}}),
+        #"Changed Type1" = Table.TransformColumnTypes(#"Sorted Rows1",{{"EFFDATE", type date}})
+
 in
     #"Changed Type1"
